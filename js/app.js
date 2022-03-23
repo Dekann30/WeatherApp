f617c3996fd2c7f505c4f3aefa48e812dc76dd8a
Deanna K <keysdeanna@gmail.com>
2022-03-23 01:37:02 -0400

    adds user input and button click functionality

--- js/app.js
+++ js/app.js
@@ -2,5 +2,11 @@ const APIKEY = 'f6ac922f696523ee05fb92cdea87b084'
 const $input = $('input')
 const $button = $('button')
 
+$button.on('click', () => {
+    const city = $input.val()
 
-$.ajax()
+    $.ajax(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIKEY}`). then((data) => {
+        console.log(data)
+    })
+})
+    
