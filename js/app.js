42826a20c890f658faf1f5a145f192dc2b97a2b6
Deanna K <keysdeanna@gmail.com>
2022-03-23 10:41:44 -0400

    adds tempurature, feels like and weather to html through JS

--- js/app.js
+++ js/app.js
@@ -6,11 +6,26 @@ $button.on('click', () => {
     const city = $input.val()
 
     $.ajax(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIKEY}`). then((data) => {
-        console.log(data)
         const lat = data[0].lat
         const lon = data[0].lon
-        console.log(lat, lon)
         $('#city').text(data[0].name)
+
+        $.ajax(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily,alerts&units=imperial&appid=${APIKEY}`).then((data2) => {
+            $('#temp').text(`${Math.trunc(data2.current.temp)}°`)
+            $('#feels-like').text(`${Math.trunc(data2.current.feels_like)}°`)
+
+            const mainW = data2.current.weather[0].main
+            const descriptionW = data2.current.weather[0].description
+
+            const capLetters = descriptionW.split(' ')
+
+            for (let i = 0; i < capLetters.length; i+=1) {
+                capLetters[i] = capLetters[i].charAt(0).toUpperCase() + capLetters[i].substr(1)
+            }
+            
+            const descrCap = capLetters.join(' ')
+            $('#weather').text(`${mainW} - ${descrCap}`)
+        })
     })
 })
     
